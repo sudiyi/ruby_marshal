6957b657b12ef5be83a143f4225dcc6bb52ce99a
liqiang <124022747@qq.com>
2016-11-02 17:03:41 +0800

    修改demo.php autoload.php 命令空间

--- example/demo.php
+++ example/demo.php
@@ -1,9 +1,9 @@
 <?php
 require getcwd() . '/autoload.php';
-use SuDiYi\RubyMarshal\RubyMarshalLoad;
-use SuDiYi\RubyMarshal\UnEscape;
+use Sudiyi\RubyMarshal\RubyMarshalLoad;
+use Sudiyi\RubyMarshal\UnEscape;
 
-$data = 'BAh7BzoGYVsJSSIGYQY6BkVUSSIGIAY7BlRJIgZiBjsGVEkiBmMGOwZUOgZi%0AZg4xMjMxMi4xMTE%3D%0A';
+$data = 'BAhbCUkiAAY6BkVUaRFJIglzZHNhBjsAVGkX%0A';
 $data = UnEscape::toUnEscape($data);
 $rubyMarshalLoad = new RubyMarshalLoad();
 $content = base64_decode($data);
